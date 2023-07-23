38af17bf4fbb3c51b7c762fe07fe4d4cf466c1c7
hagoromo2000 <revtm000126@gmail.com>
2023-07-23 21:32:34 +0900

    ユーザーデータの取得処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
 import "./App.css";
 import { UserCard } from "./components/UserCard";
-
-const user = {
-  id: 1,
-  name: "テスト太郎",
-  email: "1234@example.com",
-  address: "東京都",
-};
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/userProfile";
 
 function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+        setUserProfiles(data);
+      });
+  };
+
   return (
     <div className="App">
-      <UserCard user={user} />
+      <button onClick={onClickFetchUser}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
