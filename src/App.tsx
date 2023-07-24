1ec4c906b130e059afd4df9866be184d636afce0
hagoromo2000 <revtm000126@gmail.com>
2023-07-24 20:09:08 +0900

    カスタムフックを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import axios from "axios";
-import { useState } from "react";
 import "./App.css";
+import { useAllUsers } from "./hooks/useAllUsers";
 import { UserCard } from "./components/UserCard";
-import { User } from "./types/api/user";
-import { UserProfile } from "./types/userProfile";
 
 function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState(false);
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
 
   const onClickFetchUser = () => {
-    setLoading(true);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`,
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    getUsers();
   };
 
   return (
